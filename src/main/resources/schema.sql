63f33a1d2059990c33cf59a05afea63a789fddf5
Lajos Kossuth <lajos.kossuth@showpad.com>
2025-11-26 10:06:49 +0100

    update schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,16 +1,18 @@
 create table if not exists grocery_lists (
         id varchar(36) primary key,
         name varchar(255) not null,
-        grocery_list_items json not null,
         created_at timestamp not null default current_timestamp,
         updated_at timestamp not null default current_timestamp
 );
+
 create table if not exists grocery_list_items (
         id varchar(36) primary key,
+        grocery_list_id varchar(36) not null,
         name varchar(255) not null,
         quantity int,
         completed boolean,
-        completed_by varChar(355),
+        completed_by varchar(255),
         created_at timestamp not null default current_timestamp,
-        updated_at timestamp not null default current_timestamp
-)
+        updated_at timestamp not null default current_timestamp,
+        foreign key (grocery_list_id) references grocery_lists(id) on delete cascade
+);
